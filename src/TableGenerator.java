e6107dae2ceed16cb7e6f77af9f4d0a84c1459b6
Quang Nguyen <s3927198@rmit.edu.vn>
2022-08-03 15:59:34 +0700

    Add table generator

---
+++ src/TableGenerator.java
@@ -0,0 +1,207 @@
+import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class TableGenerator {
+    private int PADDING_SIZE = 1;
+    private String NEW_LINE = "\n";
+    private String TABLE_JOINT_SYMBOL = "+";
+    private String TABLE_V_SPLIT_SYMBOL = "|";
+    private String TABLE_H_SPLIT_SYMBOL = "-";
+
+    public String generateTable(List<String> headersList, List<List<String>> rowsList, int... overRiddenHeaderHeight) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int rowHeight = overRiddenHeaderHeight.length > 0 ? overRiddenHeaderHeight[0] : 1;
+
+        Map<Integer, Integer> columnMaxWidthMapping = getMaximumWidhtofTable(headersList, rowsList);
+
+        stringBuilder.append(NEW_LINE);
+        stringBuilder.append(NEW_LINE);
+        createRowLine(stringBuilder, headersList.size(), columnMaxWidthMapping);
+        stringBuilder.append(NEW_LINE);
+
+        for (int headerIndex = 0; headerIndex < headersList.size(); headerIndex++) {
+            fillCell(stringBuilder, headersList.get(headerIndex), headerIndex, columnMaxWidthMapping);
+        }
+
+        stringBuilder.append(NEW_LINE);
+
+        createRowLine(stringBuilder, headersList.size(), columnMaxWidthMapping);
+
+        for (List<String> row : rowsList) {
+
+            for (int i = 0; i < rowHeight; i++) {
+                stringBuilder.append(NEW_LINE);
+            }
+
+            for (int cellIndex = 0; cellIndex < row.size(); cellIndex++) {
+                fillCell(stringBuilder, row.get(cellIndex), cellIndex, columnMaxWidthMapping);
+            }
+
+        }
+
+        stringBuilder.append(NEW_LINE);
+        createRowLine(stringBuilder, headersList.size(), columnMaxWidthMapping);
+        stringBuilder.append(NEW_LINE);
+        stringBuilder.append(NEW_LINE);
+
+        return stringBuilder.toString();
+    }
+
+    private void fillSpace(StringBuilder stringBuilder, int length) {
+        for (int i = 0; i < length; i++) {
+            stringBuilder.append(" ");
+        }
+    }
+
+    private void createRowLine(StringBuilder stringBuilder, int headersListSize,
+            Map<Integer, Integer> columnMaxWidthMapping) {
+        for (int i = 0; i < headersListSize; i++) {
+            if (i == 0) {
+                stringBuilder.append(TABLE_JOINT_SYMBOL);
+            }
+
+            for (int j = 0; j < columnMaxWidthMapping.get(i) + PADDING_SIZE * 2; j++) {
+                stringBuilder.append(TABLE_H_SPLIT_SYMBOL);
+            }
+            stringBuilder.append(TABLE_JOINT_SYMBOL);
+        }
+    }
+
+    private Map<Integer, Integer> getMaximumWidhtofTable(List<String> headersList, List<List<String>> rowsList) {
+        Map<Integer, Integer> columnMaxWidthMapping = new HashMap<>();
+
+        for (int columnIndex = 0; columnIndex < headersList.size(); columnIndex++) {
+            columnMaxWidthMapping.put(columnIndex, 0);
+        }
+
+        for (int columnIndex = 0; columnIndex < headersList.size(); columnIndex++) {
+
+            if (headersList.get(columnIndex).length() > columnMaxWidthMapping.get(columnIndex)) {
+                columnMaxWidthMapping.put(columnIndex, headersList.get(columnIndex).length());
+            }
+        }
+
+        for (List<String> row : rowsList) {
+
+            for (int columnIndex = 0; columnIndex < row.size(); columnIndex++) {
+
+                if (row.get(columnIndex).length() > columnMaxWidthMapping.get(columnIndex)) {
+                    columnMaxWidthMapping.put(columnIndex, row.get(columnIndex).length());
+                }
+            }
+        }
+
+        for (int columnIndex = 0; columnIndex < headersList.size(); columnIndex++) {
+
+            if (columnMaxWidthMapping.get(columnIndex) % 2 != 0) {
+                columnMaxWidthMapping.put(columnIndex, columnMaxWidthMapping.get(columnIndex) + 1);
+            }
+        }
+
+        return columnMaxWidthMapping;
+    }
+
+    private int getOptimumCellPadding(int cellIndex, int datalength, Map<Integer, Integer> columnMaxWidthMapping,
+            int cellPaddingSize) {
+        if (datalength % 2 != 0) {
+            datalength++;
+        }
+
+        if (datalength < columnMaxWidthMapping.get(cellIndex)) {
+            cellPaddingSize = cellPaddingSize + (columnMaxWidthMapping.get(cellIndex) - datalength) / 2;
+        }
+
+        return cellPaddingSize;
+    }
+
+    private void fillCell(StringBuilder stringBuilder, String cell, int cellIndex,
+            Map<Integer, Integer> columnMaxWidthMapping) {
+
+        int cellPaddingSize = getOptimumCellPadding(cellIndex, cell.length(), columnMaxWidthMapping, PADDING_SIZE);
+
+        if (cellIndex == 0) {
+            stringBuilder.append(TABLE_V_SPLIT_SYMBOL);
+        }
+
+        fillSpace(stringBuilder, cellPaddingSize);
+        stringBuilder.append(cell);
+        if (cell.length() % 2 != 0) {
+            stringBuilder.append(" ");
+        }
+
+        fillSpace(stringBuilder, cellPaddingSize);
+
+        stringBuilder.append(TABLE_V_SPLIT_SYMBOL);
+
+    }
+
+    public static void printTable(File file, int ID) {
+        TableGenerator tableGenerator = new TableGenerator();
+        List<String> headersList = new ArrayList<>();
+        List<List<String>> rowsList = new ArrayList<>();
+
+        try {
+            Scanner sc = new Scanner(file);
+            String[] headers = sc.nextLine().split(",");
+            for (String header : headers) {
+                headersList.add(header);
+            }
+            int count = 1;
+            while (sc.hasNext()) {
+                String[] line = sc.nextLine().split(",");
+                List<String> row = new ArrayList<>();
+                if (count == ID) {
+
+                    for (int i = 0; i < line.length; i++) {
+                        row.add(line[i]);
+                    }
+                    rowsList.add(row);
+                    break;
+                } else {
+                    count++;
+                    continue;
+                }
+            }
+        } catch (FileNotFoundException e) {
+
+            e.printStackTrace();
+        }
+
+        System.out.println(tableGenerator.generateTable(headersList, rowsList));
+        // System.out.println(index);
+    }
+
+    public static void printTable(File file) {
+        TableGenerator tableGenerator = new TableGenerator();
+        List<String> headersList = new ArrayList<>();
+        List<List<String>> rowsList = new ArrayList<>();
+
+        try {
+            Scanner sc = new Scanner(file);
+            String[] headers = sc.nextLine().split(",");
+            for (String header : headers) {
+                headersList.add(header);
+            }
+            while (sc.hasNext()) {
+                String[] line = sc.nextLine().split(",");
+                List<String> row = new ArrayList<>();
+                for (int i = 0; i < line.length; i++) {
+                    row.add(line[i]);
+                }
+                rowsList.add(row);
+            }
+        } catch (FileNotFoundException e) {
+
+            e.printStackTrace();
+        }
+
+        System.out.println(tableGenerator.generateTable(headersList, rowsList));
+    }
+
+}
